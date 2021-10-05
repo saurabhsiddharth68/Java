814a022ff904ffbe17e3751605bfa5c7904635b0
Saurabh Siddharth <saurabhsiddharth68>
2021-10-05 14:05:10 +0530

    Full and half Time

--- EmployeeWageCalculate.java
+++ EmployeeWageCalculate.java
@@ -1,12 +1,16 @@
 public class EmployeeWageCalculate {
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;
+        int IS_HALF_TIME = 4;
         int EMP_RATE_PER_HOUR = 20;
+
         int empHour = 0;
         int empWage = 0;
         double empCheck =Math.floor(Math.random() * 10 ) % 2;
         if ( empCheck == IS_FULL_TIME )
             empHour = 8;
+        else if ( empCheck == IS_HALF_TIME );
+            empHour = 4;
         empWage = empHour * EMP_RATE_PER_HOUR;
         System.out.println("Emp Wage" + empWage );
             
