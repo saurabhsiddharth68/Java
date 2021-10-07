37aa5ce25481d27a48c37b2095d1a62971cecd4f
Saurabh Siddharth <saurabhsiddharth68>
2021-10-07 12:50:59 +0530

    Power Of Two

--- Powerof2.java
+++ Powerof2.java
@@ -1,13 +1,16 @@
-public class Power {
+import java.util.Scanner;
+
+public class PowerOf2 {
     public static void main(String[] args) {
-        Utility u = new Utility();
+        int num;
         System.out.println("Enter a number:");
-        int num = u.inputInteger();
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
         System.out.println("Power of 2^" + num + " is: " + (Math.pow(2, num)));
         System.out.println();
         System.out.println("Printing all till Power Value " + num);
 
-        for (int i = 1; i <= num; i++) {
+        for (int i = 1; i <= 1; i++) {
             System.out.println("Power of 2^" + i + " is: " + (Math.pow(2, i)));
         }
     }
